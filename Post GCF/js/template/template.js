c714f4a8fe89012c1fcca064a8d2e5e11cefc742
Dzikri7 <93994470+Dzikri7@users.noreply.github.com>
2024-02-02 14:36:54 +0700

    pushh

--- Post GCF/js/template/template.js
+++ Post GCF/js/template/template.js
@@ -1,5 +1,5 @@
-export let URLGeoJson = "https://us-central1-vivid-vent-401501.cloudfunctions.net/function-2";
-export let urlPostGCF = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/test-post";
+export let URLGeoJson = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/gisgetall";
+export let urlPostGCF = "https://asia-southeast2-vivid-vent-401501.cloudfunctions.net/gisgetall";
 export let tableTag="tr";
 export let tableRowClass="content is-small";
 export let tableTemplate=`
